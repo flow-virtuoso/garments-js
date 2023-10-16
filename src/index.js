25102e0b856e0e9710f9a1fa2279cf2997d7465a
Rory Stevens <132132054+rorydev24@users.noreply.github.com>
2023-10-16 14:50:46 +0100

    Completed code for Garments Homepage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { check } from 'prettier';
-
 //  ---------
 //   Sliders
 //  ---------
@@ -15,6 +13,7 @@ let cardPosition = 0;
 let isAnimating = false;
 
 // Adding event listeners
+
 document
   .querySelector('.best-sellers-slider_left-button')
   .addEventListener('click', bestSellersMoveLeft);
@@ -218,21 +217,30 @@ window.addEventListener('resize', function () {
 
 function addSwipeInteraction(swipeArea) {
   var startCoordinates = {};
+  var isSwiping = false; // Variable to track whether a swipe is in progress
 
   swipeArea.addEventListener('touchstart', function (event) {
     startCoordinates.x = event.touches[0].clientX;
     startCoordinates.y = event.touches[0].clientY;
+    isSwiping = false; // Initialize isSwiping to false
   });
 
   swipeArea.addEventListener('touchmove', function (event) {
-    event.preventDefault(); // Prevent scrolling while swiping
+    var deltaX = event.touches[0].clientX - startCoordinates.x;
+    var deltaY = event.touches[0].clientY - startCoordinates.y;
+
+    // Check if the swipe is more horizontal than vertical
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      event.preventDefault(); // Prevent scrolling while swiping horizontally
+      isSwiping = true; // Set isSwiping to true when a horizontal swipe is detected
+    }
   });
 
   swipeArea.addEventListener('touchend', function (event) {
-    var deltaX = event.changedTouches[0].clientX - startCoordinates.x;
-    var deltaY = event.changedTouches[0].clientY - startCoordinates.y;
+    if (isSwiping) {
+      // Check if a horizontal swipe was detected
+      var deltaX = event.changedTouches[0].clientX - startCoordinates.x;
 
-    if (Math.abs(deltaX) > Math.abs(deltaY)) {
       if (deltaX > 0) {
         // Right swipe
         if (swipeArea === bestSellersSwipeArea) {
@@ -251,14 +259,6 @@ function addSwipeInteraction(swipeArea) {
         } else if (swipeArea === igPostsSwipeArea) {
           igPostsMoveRight();
         }
-      }
-    } else {
-      if (deltaY > 0) {
-        // Down swipe
-        // console.log('Swiped down');
-      } else {
-        // Up swipe
-        // console.log('Swiped up');
       }
     }
   });
@@ -339,104 +339,38 @@ document.addEventListener('DOMContentLoaded', function () {
 //   Footer Accordian Menu
 //  ------------------------
 
-// Initializing an array to store the open/close state of each accordion item
-let accordionItemIsOpen = [false, true, true, true];
+// Get all elements with the attribute fs-accordion-element="arrow"
+const arrowElements = document.querySelectorAll('[fs-accordion-element="arrow"]');
 
-// Initializing an array for accordion content
-const accordionContent = document.querySelectorAll('[accordion-content]');
-const accordionArrow = document.querySelectorAll('[accordion-arrow]');
-
-// Take a snapshot of the height of each accordion content
-let contentHeight = [];
-let contentPaddingTop = [];
-let contentPaddingBottom = [];
-
-function updateMeasurementArrays() {
-  contentHeight = [];
-  contentPaddingTop = [];
-  contentPaddingBottom = [];
-
-  accordionContent.forEach((element) => {
-    let height = element.offsetHeight;
-    let computedStyle = getComputedStyle(element);
-    let elementPaddingTop = parseFloat(computedStyle.paddingTop);
-    let elementPaddingBottom = parseFloat(computedStyle.paddingBottom);
-    contentHeight.push(height);
-    contentPaddingTop.push(elementPaddingTop);
-    contentPaddingBottom.push(elementPaddingBottom);
-  });
-
-  console.log(contentHeight);
-  console.log(contentPaddingTop);
-  console.log(contentPaddingBottom);
-}
-
-//initial snapshot of measurements
-updateMeasurementArrays();
-
-let resizeTimer;
-let isResizing = false;
-
-function handleResize() {
-  if (isResizing) {
-    clearTimeout(resizeTimer);
-  } else {
-    isResizing = true;
-  }
-
-  resizeTimer = setTimeout(() => {
-    isResizing = false;
-    updateMeasurementArrays();
-  }, 1000); // Adjust the debounce time as needed
-}
-
-window.addEventListener('resize', handleResize);
-
-// Function to toggle the state of an accordion item based on its index
-function accordionButtonClick(accordionItemNumber) {
-  // Get the index for the accordion item (0-based)
-  const itemIndex = accordionItemNumber - 1;
-
-  // Iterate through all accordion items
-  accordionItemIsOpen.forEach((isOpen, index) => {
-    if (index === itemIndex) {
-      // if its the accordion item you clicked
-      if (accordionItemIsOpen[itemIndex]) {
-        // and its open, then close it
-        accordionContent[itemIndex].style.maxHeight = '0px';
-        accordionContent[itemIndex].style.paddingTop = '0px';
-        accordionContent[itemIndex].style.paddingBottom = '0px';
-        accordionArrow[itemIndex].style.transform = 'rotateZ(0deg)';
-        accordionItemIsOpen[itemIndex] = !accordionItemIsOpen[itemIndex];
-      } else {
-        // and its closed, then open it
-        accordionContent[itemIndex].style.maxHeight = contentHeight[itemIndex] + 'px';
-        accordionContent[itemIndex].style.paddingTop = contentPaddingTop[itemIndex] + 'px';
-        accordionContent[itemIndex].style.paddingBottom = contentPaddingBottom[itemIndex] + 'px';
-        accordionArrow[itemIndex].style.transform = 'rotateZ(180deg)';
-        accordionItemIsOpen[itemIndex] = !accordionItemIsOpen[itemIndex];
+// Create a MutationObserver for each element
+arrowElements.forEach((arrowElement) => {
+  const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+        if (arrowElement.classList.contains('is-active-accordion')) {
+          // Find and set the opacity of the child element with fs-accordion-element="horizontal-line"
+          const horizontalLine = arrowElement.querySelector(
+            '[fs-accordion-element="horizontal-line"]'
+          );
+          if (horizontalLine) {
+            horizontalLine.style.opacity = 0;
+          }
+        } else {
+          // Reset the opacity when is-active-accordion is removed
+          const horizontalLine = arrowElement.querySelector(
+            '[fs-accordion-element="horizontal-line"]'
+          );
+          if (horizontalLine) {
+            horizontalLine.style.opacity = 1;
+          }
+        }
       }
-    } else if (isOpen) {
-      // if its not the accordion item you clicked
-      accordionContent[index].style.maxHeight = '0px';
-      accordionContent[index].style.paddingTop = '0px';
-      accordionContent[index].style.paddingBottom = '0px';
-      accordionArrow[index].style.transform = 'rotateZ(0deg)';
-      accordionItemIsOpen[index] = false;
     }
   });
-  // check the status of which items are open
-  console.log(accordionItemIsOpen);
-}
 
-// Adding event listeners to buttons
-const accordionButtons = document.querySelectorAll('[id^="accordion-button-"]');
+  // Define the configuration for the MutationObserver
+  const config = { attributes: true, attributeFilter: ['class'] };
 
-accordionButtons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    accordionButtonClick(index + 1);
-  });
+  // Start observing each target element
+  observer.observe(arrowElement, config);
 });
-
-// Closing the accordion items when the page loads
-accordionButtonClick(1);
